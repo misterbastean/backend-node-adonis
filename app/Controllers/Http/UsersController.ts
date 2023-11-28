aa5f0e6a1abaa339246b3ec76426bae7fe33a470
Josh Bastean <joshua.bastean@endava.com>
2023-11-28 12:03:26 -0500

    Add UPDATE user

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -68,18 +68,40 @@ export default class UsersController {
         error: err.message,
       };
     }
-    return {
-      message: "Show single user",
-      params,
-    };
   }
 
-  public async update({ params, request }: HttpContextContract) {
-    return {
-      message: "Update single user",
-      params,
-      body: request.body(),
-    };
+  public async update({ params, request, response }: HttpContextContract) {
+    try {
+      const user = await User.find(params.id);
+      if (user) {
+        const data = request.body().data;
+
+        // TODO: Validate data
+
+        user.merge(data);
+        await user.save();
+
+        return {
+          code: 0,
+          data: {
+            id: user.id,
+          },
+        };
+      } else {
+        response.status(404);
+        return {
+          code: 0,
+          data: null,
+        };
+      }
+    } catch (err) {
+      response.status(500);
+      console.error("error:", err);
+      return {
+        code: err.errno || 0,
+        error: err.message,
+      };
+    }
   }
 
   public async destroy({ params }: HttpContextContract) {
